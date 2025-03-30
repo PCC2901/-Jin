494eee738127619038259b46441c1fe72286dcaa
PCC2901 <cuongazgamer@gmail.com>
2025-03-30 16:24:13 +0700

    Update AntiAFK.lua

--- AntiAFK.lua
+++ AntiAFK.lua
@@ -8,6 +8,10 @@ local player = Players.LocalPlayer
 local mouse = player:GetMouse()
 local autoClick, clickPosition, clickConnection = false, Vector2.new(mouse.X, mouse.Y), nil
 
+if not player:GetAttribute("autoMacro") then
+    player:SetAttribute("autoMacro", false)
+end
+
 local ScreenGui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
 local ClickFrame = Instance.new("Frame", ScreenGui)
 ClickFrame.Size = UDim2.new(0, 50, 0, 50)
@@ -30,11 +34,12 @@ end)
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
     if not gameProcessed and input.KeyCode == Enum.KeyCode.L then
         autoClick = not autoClick
+        player:SetAttribute("autoMacro", autoClick)
         ClickFrame.BackgroundColor3 = autoClick and Color3.fromRGB(0, 255, 0) or Color3.fromRGB(255, 0, 0)
         if autoClick then
             clickConnection = RunService.RenderStepped:Connect(onRenderStep)
         elseif clickConnection then
-            clickConnection:Disconnect() 
+            clickConnection:Disconnect()
             clickConnection = nil
         end
         StarterGui:SetCore("SendNotification", {Title = "Auto Clicker", Text = autoClick and "Bật Auto Click" or "Tắt Auto Click", Duration = 2})
